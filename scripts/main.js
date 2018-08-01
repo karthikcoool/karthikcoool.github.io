b55155ccd13f686f3977aae81b8064dded86e4c6
karthikcoool <31313595+karthikcoool@users.noreply.github.com>
2018-08-01 13:50:00 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+﻿var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+var myVar = setInterval(myTimer, 10000);
+var i=0;
+var k;
+function myTimer()
+{   
+	 alert(i);
+
+ i++;
+ 
+
+}
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
